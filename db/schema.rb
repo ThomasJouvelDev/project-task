fa3c3b44f366e973c0c4a19896b6b480f7c82fb9
thomas jouvel <thomas.jouvel@gmail.com>
2023-01-20 17:14:29 +0100

    docker run en local

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_20_145813) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.boolean "done"
+    t.datetime "due_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
