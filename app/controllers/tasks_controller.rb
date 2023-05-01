0e09c9c6349a5fc4e315362ba9b6eaa3ff922bc4
thomas jouvel <thomas.jouvel@gmail.com>
2023-05-01 18:00:06 +0200

    add fancy checkbox

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -65,6 +65,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:name, :content, :done, :due_date)
+      params.permit(:name, :content, :done, :due_date)
     end
 end
